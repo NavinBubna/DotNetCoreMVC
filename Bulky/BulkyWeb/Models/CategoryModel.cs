1d06f1e416f1de506ce56d50c2b876e71d7a4d9a
Navin Bubna <navin@esopdirect.com>
2024-01-10 22:46:31 +0530

    Section2 24.Data Annotation (Primary Key, Required, etc.)

--- Bulky/BulkyWeb/Models/CategoryModel.cs
+++ Bulky/BulkyWeb/Models/CategoryModel.cs
@@ -1,8 +1,12 @@
-﻿namespace BulkyWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
 {
     public class CategoryModel
     {
+        [Key]   // Id or {className}Id is the default name for primary key. Other than that we can also use [Key] annotation
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
