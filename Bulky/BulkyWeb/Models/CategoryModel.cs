cfa25872718abbdde23ffcf6d1fa093ad3da19ca
Navin Bubna <navin@esopdirect.com>
2024-01-23 20:35:26 +0530

    Section2 39:Input Tag Helpers

--- Bulky/BulkyWeb/Models/CategoryModel.cs
+++ Bulky/BulkyWeb/Models/CategoryModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
@@ -7,7 +8,9 @@ namespace BulkyWeb.Models
         [Key]   // Id or {className}Id is the default name for primary key. Other than that we can also use [Key] annotation
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
