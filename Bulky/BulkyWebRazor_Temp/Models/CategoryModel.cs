8e8864c74c89456519125013d3f97126220fc35f
Navin Bubna <navin@esopdirect.com>
2024-01-30 23:09:56 +0530

    Section3 54&55:What is Different in Razor Project

---
+++ Bulky/BulkyWebRazor_Temp/Models/CategoryModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class CategoryModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(20)]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(0, 100, ErrorMessage = "The field Display Order must be between 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
