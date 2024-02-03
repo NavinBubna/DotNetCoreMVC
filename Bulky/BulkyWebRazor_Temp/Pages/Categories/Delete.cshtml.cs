2afa46ddb4f0bd4f082c4ad6ed9afb9fb3d220e4
Navin Bubna <navin@esopdirect.com>
2024-02-03 18:51:11 +0530

    Section3 61:Edit and Delete Category

---
+++ Bulky/BulkyWebRazor_Temp/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,39 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryModel Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet(int? id)
+        {
+            if (id != null && id > 0)
+            {
+                Category = _db.Category.FirstOrDefault(u => u.Id == id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            CategoryModel? obj = _db.Category.Find(Category.Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
+        }
+    }
+}
--- Bulky/BulkyWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
+++ Bulky/BulkyWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
@@ -6,24 +6,29 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace BulkyWebRazor_Temp.Pages.Categories
 {
     [BindProperties]
-    public class CreateModel : PageModel
+    public class EditModel : PageModel
     {
         private readonly ApplicationDbContext _db;
         public CategoryModel Category { get; set; }
-        public CreateModel(ApplicationDbContext db)
+
+        public EditModel(ApplicationDbContext db)
         {
             _db = db;
         }
 
-        public void OnGet()
+        public void OnGet(int? id)
         {
+            if (id != null && id > 0)
+            {
+                Category = _db.Category.FirstOrDefault(u => u.Id == id);
+            }
         }
 
         public IActionResult OnPost()
         {
             if (ModelState.IsValid)
             {
-                _db.Category.Add(Category);
+                _db.Category.Update(Category);
                 _db.SaveChanges();
                 return RedirectToPage("Index");
             }
