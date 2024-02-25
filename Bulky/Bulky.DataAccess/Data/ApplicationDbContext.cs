188ecc93c9fd81b3e7877bf59eccefa981975dca
Navin Bubna <navin@esopdirect.com>
2024-02-25 17:34:17 +0530

    Section6 86: Add Foreign Key in EF Core

--- Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -32,7 +32,8 @@ namespace BulkyBook.DataAccess.Data
                     ListPrice = 99,
                     Price = 90,
                     Price50 = 85,
-                    Price100 = 80
+                    Price100 = 80,
+                    CategoryId = 1
                 },
                 new ProductModel
                 {
@@ -44,7 +45,8 @@ namespace BulkyBook.DataAccess.Data
                     ListPrice = 40,
                     Price = 30,
                     Price50 = 25,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId = 1
                 },
                 new ProductModel
                 {
@@ -56,7 +58,8 @@ namespace BulkyBook.DataAccess.Data
                     ListPrice = 55,
                     Price = 50,
                     Price50 = 40,
-                    Price100 = 35
+                    Price100 = 35,
+                    CategoryId = 1
                 },
                 new ProductModel
                 {
@@ -68,7 +71,8 @@ namespace BulkyBook.DataAccess.Data
                     ListPrice = 70,
                     Price = 65,
                     Price50 = 60,
-                    Price100 = 55
+                    Price100 = 55,
+                    CategoryId = 2
                 },
                 new ProductModel
                 {
@@ -80,7 +84,8 @@ namespace BulkyBook.DataAccess.Data
                     ListPrice = 30,
                     Price = 27,
                     Price50 = 25,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId = 2
                 },
                 new ProductModel
                 {
@@ -92,7 +97,8 @@ namespace BulkyBook.DataAccess.Data
                     ListPrice = 25,
                     Price = 23,
                     Price50 = 22,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId = 3
                 }
                 );
         }
--- Bulky/Bulky.Models/ProductModel.cs
+++ Bulky/Bulky.Models/ProductModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -37,5 +38,9 @@ namespace BulkyBook.Models
         [Range(1, 1000)]
         [Display(Name = "Price for 100+")]
         public double Price100 { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public CategoryModel Category { get; set; }
     }
 }
