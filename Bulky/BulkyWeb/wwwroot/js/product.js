174e2e5396cd93e73abe0e00cd3f035afafb8e2e
Navin Bubna <navin@esopdirect.com>
2024-03-02 20:50:50 +0530

    Section6 104: Edit Product Link in DataTable

--- Bulky/BulkyWeb/wwwroot/js/product.js
+++ Bulky/BulkyWeb/wwwroot/js/product.js
@@ -9,8 +9,20 @@ function loadDataTable() {
             { data: 'title'},
             { data: 'isbn', "width": "15%" },
             { data: 'listPrice', "width": "10%" },
-            { data: 'author', "width": "20%" },
-            { data: 'category.name', "width": "15%" }
+            { data: 'author', "width": "15%" },
+            { data: 'category.name', "width": "10%" },
+            {
+                data: 'id',
+                render: `<div class="w-75 btn-group" role="group">
+                            <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2">
+                                <i class="bi bi-pencil-square"></i> Edit
+                            </a>
+                            <a href="" class="btn btn-danger mx-2">
+                                <i class="bi bi-trash-fill"></i> Delete
+                            </a>
+                         </div>`,
+                "width": "25%"
+            }
         ]
     });
 }
