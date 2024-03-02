02bdd6a5c038ab41ae61a1d36d3a9a5772889060
Navin Bubna <navin@esopdirect.com>
2024-03-02 20:16:38 +0530

    Section6 102: Load DataTables

--- Bulky/BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ Bulky/BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -145,7 +145,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         }
 
         #region API CALLS
-
+        [HttpGet]
         public IActionResult GetAll()
         {
             IEnumerable<ProductModel> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
