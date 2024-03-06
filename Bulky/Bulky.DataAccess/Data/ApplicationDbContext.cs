16ecb447de214c9881fce6ef5a0f53042f282202
Navin Bubna <navin@esopdirect.com>
2024-03-06 20:47:13 +0530

    Section8 116: Register a User

--- Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky/Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace BulkyBook.DataAccess.Data
 
         public DbSet<CategoryModel> Categories { get; set; }
         public DbSet<ProductModel> Products { get; set; }
+        public DbSet<ApplicationUsers> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
