e1c14358edd0eae3b3cdee6b8348600d7527b870
Navin Bubna <navin@esopdirect.com>
2024-04-10 22:34:53 +0530

    Section10 131: Add Shopping Cart to Repository

--- Bulky/Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ Bulky/Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -13,6 +13,7 @@ namespace BulkyBook.DataAccess.Repository.IRepository
         ICategoryRepository Category { get; }
         IProductRepository Product { get; }
         ICompanyRepository Company { get; }
+        IShoppingCartRepository ShoppingCart { get; }
         void Save();
     }
 }
