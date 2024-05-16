bf3f55c0283546c9548dffbe854c280cf02207c6
Navin Bubna <navin@esopdirect.com>
2024-05-16 21:41:00 +0530

    Section11 153: Add Helper Methods in Order Header Repository

--- Bulky/Bulky.DataAccess/Repository/OrderHeaderRepository.cs
+++ Bulky/Bulky.DataAccess/Repository/OrderHeaderRepository.cs
@@ -9,17 +9,47 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.DataAccess.Repository
 {
-    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
-    {
-        private readonly ApplicationDbContext _db;
-        public OrderHeaderRepository(ApplicationDbContext db) : base(db)
-        {
-            _db = db;
-        }
+	public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+	{
+		private readonly ApplicationDbContext _db;
+		public OrderHeaderRepository(ApplicationDbContext db) : base(db)
+		{
+			_db = db;
+		}
 
-        public void Update(OrderHeader obj)
-        {
-            _db.OrderHeaders.Update(obj);
-        }
-    }
+		public void Update(OrderHeader obj)
+		{
+			_db.OrderHeaders.Update(obj);
+		}
+
+		public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+		{
+			var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+			if (orderFromDb != null)
+			{
+				orderFromDb.OrderStatus = orderStatus;
+				if (!string.IsNullOrEmpty(paymentStatus))
+				{
+					orderFromDb.PaymentStatus = paymentStatus;
+				}
+			}
+		}
+
+		public void UpdateStipePaymentId(int id, string sessionId, string paymentIntentId)
+		{
+			var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+			if (orderFromDb != null)
+			{
+				if (!string.IsNullOrEmpty(sessionId))
+				{
+					orderFromDb.SessionId = sessionId;
+				}
+				if (!string.IsNullOrEmpty(paymentIntentId))
+				{
+					orderFromDb.PaymentIntentId = paymentIntentId;
+					orderFromDb.OrderDate = DateTime.Now;
+				}
+			}
+		}
+	}
 }
--- Bulky/Bulky.Models/OrderHeader.cs
+++ Bulky/Bulky.Models/OrderHeader.cs
@@ -20,6 +20,7 @@ namespace BulkyBook.Models
         public DateTime ShippingDate { get; set; }
         public double OrderTotal { get; set; }
         public string? OrderStatus { get; set; }
+        public string? SessionId { get; set; }
         public string? PaymentStatus { get; set; }
         public string? TrackingNumber { get; set; }
         public string? Carrier { get; set; }
