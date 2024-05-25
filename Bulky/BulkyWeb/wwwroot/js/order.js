73e9718546fbaf1b867f53e10bb2aeb4c328624d
Navin Bubna <navinbubna@gmail.com>
2024-05-25 12:58:08 +0530

    Section12 161: Demo - Filters in Order List

--- Bulky/BulkyWeb/wwwroot/js/order.js
+++ Bulky/BulkyWeb/wwwroot/js/order.js
@@ -1,18 +1,34 @@
 ﻿var dataTable
 
 $(document).ready(function () {
-    loadDataTable();
+    var url = window.location.search;
+
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else if (url.includes("completed")) {
+        loadDataTable("completed");
+    }
+    else if (url.includes("pending")) {
+        loadDataTable("pending");
+    }
+    else if (url.includes("approved")) {
+        loadDataTable("approved");
+    }
+    else {
+        loadDataTable("all");
+    }
 });
 
-function loadDataTable() {
+function loadDataTable(status) {
     dataTable = $('#tblData').DataTable({
-        "ajax": { url: '/admin/order/getall' },
+        "ajax": { url: '/admin/order/getall?status=' + status },
         "columns": [
             { data: 'id', "width": "5%" },
-            { data: 'name', "width": "20%" },
-            { data: 'phoneNumber', "width": "15%" },
+            { data: 'name', "width": "25%" },
+            { data: 'phoneNumber', "width": "20%" },
             { data: 'applicationUser.email', "width": "20%" },
-            { data: 'orderStatus', "width": "15%" },
+            { data: 'orderStatus', "width": "10%" },
             { data: 'orderTotal', "width": "10%"},
             {
                 data: 'id',
@@ -23,7 +39,7 @@ function loadDataTable() {
                             </a>
                          </div>`
                 },
-                "width": "25%"
+                "width": "10%"
             }
         ]
     });
